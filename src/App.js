ca5850aff73ae43a5dbb999356f85b774253471f
Onepain <onepaintdap@gmail.com>
2022-06-23 19:33:56 +0100

    initial setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './app.css';
+
+
+//Assets
+import {bgDesktopDark, bgDesktopLight} from './assets/assets'
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/assets.js
@@ -0,0 +1,10 @@
+
+export {default as bgDesktopDark} from './bg-desktop-dark.jpg'
+export {default as bgDesktopLight} from './bg-desktop-light.jpg'
+export {default as bgMobileLight} from './bg-mobile-light.jpg'
+export {default as bgMobileDark} from './bg-mobile-dark.jpg'
+export {default as favicon} from './favicon-32x32.png'
+export {default as iconCheck} from './icon-check.svg'
+export {default as iconCross} from './icon-cross.svg'
+export {default as iconMoon} from './icon-moon.svg'
+export {default as iconSun} from './icon-sun.svg'
